dc2ca1f947912cfb19ed32f52905e92feb5f62f6
VictorRodriguezContact <victorrodriguezcontact@gmail.com>
2024-07-02 21:09:26 +0100

    Mock controller created + WorkerModel

--- service/src/main/java/controller/WorkerRestController.java
+++ service/src/main/java/controller/WorkerRestController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import model.WorkerModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class WorkerRestController {
+
+    @GetMapping("/worker")
+    public List<WorkerModel> getWorkers(@RequestParam(value = "id", defaultValue = "") String id) {
+        return null;
+    }
 }
